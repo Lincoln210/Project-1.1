9b1cab3684ea6a8fa52021f2849a551f3759b246
Lincoln Saha <110119281+Lincoln210@users.noreply.github.com>
2024-01-31 12:05:38 +0800

    Changed approach to implementing search algos only

--- escape_the_maze.py
+++ escape_the_maze.py
@@ -1,7 +1,18 @@
-class Maze:
-    def __init__(self, maze_dict):
-        self.rows = maze_dict["rows"]
-        self.cols = maze_dict["cols"]
-        self.obstacles = set(tuple(obstacles) for obstacles in maze_dict["obstacles"])
-        self.start = tuple(maze_dict["start"])
-        self.goals = [tuple(goal) for goal in maze_dict["goals"]]
+def dfs_search(maze_dict):
+    frontier = [] # frontier is a stack in DFS
+    visited = set()
+    start = maze_dict["start"]
+    frontier.append(start, [start]) # the first element in this tuple keeps track of the node at the top of the stack, and the second element keeps track of the path taken to get to that node
+
+    while frontier:
+        position, path = frontier.pop()
+        if position not in visited:
+            visited.add(position)
+
+        if position in maze_dict["goals"]:
+            return path
+        
+        for neighbor in neighbors(position, maze_dict):
+            if neighbor not in visited:
+                frontier.append(neighbor, path.append(neighbor))
+
